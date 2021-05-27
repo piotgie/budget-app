1055db8efb13054daa5a7ffda362afdf8fc3efe1
Piotr Gaik <piotgie@gmail.com>
2021-05-27 10:02:15 +0200

    init

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,93 @@
+import React, {useState, useContext} from 'react';
+import {v4 as uuidv4} from 'uuid';
+import {GlobalContext} from '../context/GlobalState';
+
+function AddTransaction() {
+
+    const {addIncome, addExpense} = useContext(GlobalContext)
+
+    const [income, setIncome] = useState({
+        incomeText: '',
+        incomeAmount: 0,
+    });
+
+    const {incomeText, incomeAmount} = income
+
+    const onChangeIncome = e => {
+        setIncome({...income,[e.target.name]: e.target.value});
+    };
+
+    const onSubmitIncome = e => {
+        e.preventDefault();
+
+        if (incomeText !== '') {
+            const newIncomeTransaction = {
+            id: uuidv4(),
+            incomeText,
+            incomeAmount: incomeAmount * 1,
+        };
+
+        addIncome(newIncomeTransaction)
+
+        setIncome({
+            incomeText: '',
+            incomeAmount: 0,
+
+        })
+        }
+
+    };
+
+    const [expense, setExpense] = useState({
+        expenseText: '',
+        expenseAmount: 0,
+    });
+
+    const {expenseText, expenseAmount} = expense
+
+    const onChangeExpense = e => {
+        setExpense({...expense,[e.target.name]: e.target.value});
+    };
+
+    const onSubmitExpense = e => {
+        e.preventDefault();
+
+        if (expenseText !== '') {
+            const newExpenseTransaction = {
+            id: uuidv4(),
+            expenseText,
+            expenseAmount: expenseAmount * 1,
+        };
+
+        addExpense(newExpenseTransaction)
+
+        setExpense({
+            expenseText: "",
+            expenseAmount: 0,
+        })
+        }
+
+
+    };
+
+    return (
+        <div className="form-wrapper">
+            <form onSubmit={onSubmitIncome}>
+                <div className="input-group income">
+                    <input type="text" value={incomeText} name="incomeText" placeholder="add income.." autoComplete="off" onChange={onChangeIncome} />
+                    <input type="number" value={incomeAmount} name="incomeAmount" placeholder="amount" autoComplete="off" onChange={onChangeIncome} />
+                    <input type="submit" value="submit"/>
+                </div>
+            </form>    
+            <form onSubmit={onSubmitExpense}>
+                <div className="input-group expense">
+                    <input type="text" value={expenseText} name="expenseText" placeholder="add expense.." autoComplete="off" onChange={onChangeExpense} />
+                    <input type="number" value={expenseAmount} name="expenseAmount" placeholder="amount" autoComplete="off" onChange={onChangeExpense} />
+                    <input type="submit" value="submit"/>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default AddTransaction
